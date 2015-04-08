dc733a55955d39a2899342d3c3914ca6c808fbeb
MrZeRo2000 <MrZeRo2000@outlook.com>
2015-04-08 13:39:58 +0300

    DBXML parser initial version

--- src/com/romanpulov/symphonytimer/DBXMLHelper.java
+++ src/com/romanpulov/symphonytimer/DBXMLHelper.java
@@ -145,10 +145,12 @@ public class DBXMLHelper {
         	xmlParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
         	xmlParser.setInput(inputStream, null);
 
+        	/*
         	//reading root
         	xmlParser.nextTag();
         	//check for correct tag
         	xmlParser.require(XmlPullParser.START_TAG, null, DBOpenHelper.DATABASE_NAME);
+        	
         	
         	int eventType = xmlParser.getEventType();
         	while (eventType != XmlPullParser.END_DOCUMENT) {
@@ -189,6 +191,158 @@ public class DBXMLHelper {
         		
         		eventType = xmlParser.next();
         	}
+        	*/
+        	
+        	
+        	String tableName = null;
+        	String tableItem = null;
+        	String fieldName = null;
+        	String fieldValue = null;
+        	int eventType = xmlParser.getEventType();
+        	int a1_s = 0;        	
+        	while ((eventType != XmlPullParser.END_DOCUMENT) && (10000 > a1_s)) {
+        		
+        		switch (a1_s) {
+        		case 0:
+        			Log.d("DBXMLHelper_parseDBXML", "case = 0");
+        			//starting        			
+        			
+                	//reading root
+                	xmlParser.next();
+                	//check for correct tag
+                	xmlParser.require(XmlPullParser.START_TAG, null, DBOpenHelper.DATABASE_NAME);
+                	a1_s = 100;
+                	break;
+                	
+        		case 100:
+        			Log.d("DBXMLHelper_parseDBXML", "case = 100");
+        			//searching for some table name
+        			
+        			xmlParser.next();
+        			Log.d("DBXMLHelper_parseDBXML", "case = 100, event = " + xmlParser.getEventType() + ", name = " + xmlParser.getName());
+        			
+        			//getting table name
+        			tableName = xmlParser.getName();
+        			//checking name
+        			if ((DBOpenHelper.TIMER_TABLE_NAME.equals(tableName)) || (DBOpenHelper.TIMER_HISTORY_TABLE_NAME.equals(tableName))) {
+        				tableItem = getTableItem(tableName);
+        				a1_s = 200;
+
+        			} else {
+        				if ((XmlPullParser.END_TAG == xmlParser.getEventType()))  {
+        					//finished reading tables
+        					a1_s = 10001;
+        					
+        				} else {
+	        				// no table name found
+	        				a1_s = 10100;
+        				}
+        			}
+        			break;     		
+        			
+        		case 200:
+        			Log.d("DBXMLHelper_parseDBXML", "case = 200, tableName = " + tableName + ", tableItem = " + tableItem);
+        			
+        			//reading table item
+        			xmlParser.next();
+        			
+        			Log.d("DBXMLHelper_parseDBXML", "case = 200, event = " + xmlParser.getEventType() + ", name = " + xmlParser.getName());
+
+        			if ((XmlPullParser.START_TAG == xmlParser.getEventType()) && (tableItem.equals(xmlParser.getName()))) {
+        				//reading table item attributes
+        				a1_s = 300;
+        			} else {
+        				if ((XmlPullParser.END_TAG == xmlParser.getEventType())) {
+        					//move to read next table
+        					a1_s = 100;
+        				} else {        				
+	        				//no table closing tag found where expected
+	        				a1_s = 10200;
+        				}
+        			}
+        			
+        			break;
+        			
+        		case 300:
+        			Log.d("DBXMLHelper_parseDBXML", "case = 300, name = " + xmlParser.getName());
+        			
+        			//reading table item attributes
+        			xmlParser.next();
+        			
+        			//attribute title
+        			if (XmlPullParser.START_TAG == xmlParser.getEventType()) {
+        				fieldName = xmlParser.getName();
+        				//move to read text
+        				a1_s = 400;
+        			} else {
+        				//start tag not found where expected
+        				if (XmlPullParser.END_TAG == xmlParser.getEventType()) {
+        					
+        					//move to read next table item
+        					a1_s = 200;
+        				} else {
+        					//unexpected event
+        					a1_s = 10300;
+        				}
+        			}
+        			
+        			break;
+        		
+        		case 400:
+        			Log.d("DBXMLHelper_parseDBXML", "case = 400, fieldName = " + fieldName);
+        			
+        			//reading field text
+        			xmlParser.next();
+        			
+        			//attribute name
+        			if (XmlPullParser.TEXT == xmlParser.getEventType()) {
+        				//read text
+        				fieldValue = xmlParser.getText();
+        				a1_s = 500;
+        				
+        			} else {
+        				//text not found where expected
+        				a1_s = 10400;
+        			}
+        			
+        			break;
+        		
+        		case 500:
+        			Log.d("DBXMLHelper_parseDBXML", "case = 500, fieldName = " + fieldName + ", fieldValue = " + fieldValue);
+        			
+        			//reading text closing tag
+        			xmlParser.next();
+        			
+        			if (XmlPullParser.END_TAG == xmlParser.getEventType()) {
+        				//move to next attribute
+        				a1_s = 300;
+        			} else {
+        				//text closing tag not found
+        				a1_s = 10500;
+        			}
+        			
+        			break;
+        			
+        		}
+        		
+        		
+        	}
+        	
+        	Log.d("DBXMLHelper_parseDBXML", "Exiting with state =" + a1_s);
+        	
+        	
+        	//log everything in the xml
+        	Log.d("DBXMLHelper_parseDBXML", "Log all xml ===================================================");
+        	eventType = xmlParser.getEventType();
+        	while (eventType != XmlPullParser.END_DOCUMENT) {
+        		
+        		Log.d("DBXMLHelper_parseDBXML", "Name = " + xmlParser.getName() + ", event = " + eventType + ", text = " + xmlParser.getText());
+        		
+        		xmlParser.next();
+        		eventType = xmlParser.getEventType();
+        	}
+
+        	
         	/*
         	
         	while (xmlParser.next() != XmlPullParser.END_TAG) {
