e6f7244ec954c7ca3372b4b6b7274d944734a4c1
MrZeRo2000 <MrZeRo2000@outlook.com>
2015-04-27 18:53:49 +0300

    new progresscircle control placeholder

---
+++ src/com/romanpulov/symphonytimer/controls/ProgressCircle.java
@@ -0,0 +1,20 @@
+package com.romanpulov.symphonytimer.controls;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ProgressCircle extends View {
+	
+	// constructor from code
+	public ProgressCircle(Context context) {
+		super(context);
+	}
+
+	// constructor from xml
+	public ProgressCircle(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
