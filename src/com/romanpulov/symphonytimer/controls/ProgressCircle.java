0a2fb9b3121b979c9992e38b095482b0c17cf16d
MrZeRo2000 <MrZeRo2000@outlook.com>
2015-04-28 19:21:13 +0300

    progresscircle update attempt to output text

--- src/com/romanpulov/symphonytimer/controls/ProgressCircle.java
+++ src/com/romanpulov/symphonytimer/controls/ProgressCircle.java
@@ -1,20 +1,49 @@
 package com.romanpulov.symphonytimer.controls;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class ProgressCircle extends View {
 	
+	private Paint mTextPaint;
+	
 	// constructor from code
 	public ProgressCircle(Context context) {
 		super(context);
+		initProgressCircle();
 	}
 
 	// constructor from xml
 	public ProgressCircle(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		initProgressCircle();
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void initProgressCircle() {
+		mTextPaint = new Paint();
+		mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setStyle(Paint.Style.STROKE);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		canvas.drawText("00", (getWidth() - (int) mTextPaint.measureText("00")) / 2 , getHeight() / 2, mTextPaint);		
+		canvas.drawRect(0, 0, getWidth(), getHeight(), mTextPaint);
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// TODO Auto-generated method stub
+		setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec));
 	}
 
 }
