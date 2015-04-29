70050397cf3276b46f07f757b851a21d8d9f497f
MrZeRo2000 <MrZeRo2000@outlook.com>
2015-04-29 14:01:33 +0300

    progresscircle basic drawing done

--- src/com/romanpulov/symphonytimer/controls/ProgressCircle.java
+++ src/com/romanpulov/symphonytimer/controls/ProgressCircle.java
@@ -1,15 +1,31 @@
 package com.romanpulov.symphonytimer.controls;
 
+import com.romanpulov.symphonytimer.R;
+
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class ProgressCircle extends View {
 	
+	private static final int ARC_MARGIN = 5;
+	private static final int ARC_THICKNESS = 5;
+	
+	private int mMin;
+	private int mMax;
+	private int mProgress;
+	
 	private Paint mTextPaint;
+	private Paint mArcPaint;
+	private Rect mTextBounds;
+	private RectF mArcRect;
 	
 	// constructor from code
 	public ProgressCircle(Context context) {
@@ -21,23 +37,105 @@ public class ProgressCircle extends View {
 	public ProgressCircle(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		initProgressCircle();
-		// TODO Auto-generated constructor stub
+		
+		//read resources
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ProgressCircle);
+        
+        int min =  a.getInteger(R.styleable.ProgressCircle_min, mMin);        
+        int max = a.getInteger(R.styleable.ProgressCircle_max, mMax);
+        int progress = a.getInteger(R.styleable.ProgressCircle_progress, mProgress);
+        
+        if ((max >= min) && (progress >= min) && (progress <= max)) {
+        	mMin = min;
+        	mMax = max;
+        	mProgress = progress;
+        }
+
+        a.recycle();
+	
 	}
 	
 	private void initProgressCircle() {
+		
+		//defaults
+		mMin = 0;
+		mMax = 100;
+		mProgress = 50;
+		
+		//paint
 		mTextPaint = new Paint();
 		mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
-        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setTextSize(12 * getResources().getDisplayMetrics().density);
+        mTextPaint.setColor(Color.WHITE);
         mTextPaint.setStyle(Paint.Style.STROKE);
+        mTextPaint.setTextAlign(Align.LEFT);
+        mTextBounds = new Rect();
+        mTextPaint.getTextBounds("000", 0, 3, mTextBounds);
+        
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(true);
+        mArcPaint.setStrokeWidth(ARC_THICKNESS);
+        mArcPaint.setStyle(Paint.Style.STROKE);
+        mArcPaint.setColor(Color.RED);
+        
+        mArcRect = new RectF();
 	}
+	
+	public void setMin(int min) {
+		if (min <= mMax) {
+			mMin = min;
+			if (mProgress < mMin) {
+				setProgress(mMin);
+			} else { 
+				invalidate();
+			}
+		}
+	}
+	
+	public void setMax(int max) {
+		if (max >= mMin) {		
+			mMax = max;
+			if (mProgress > mMax) {
+				setProgress(mMax);
+			} else {
+				invalidate();
+			}
+			invalidate();
+		}
+	}
+	
+	public void setProgress(int progress) {
+		if (progress < mMin) {
+			mProgress = mMin;
+		} else if (progress > mMax) {
+			mProgress = mMax;
+		} else
+			mProgress = progress;
+		invalidate();
+	}
+	
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
-		canvas.drawText("00", (getWidth() - (int) mTextPaint.measureText("00")) / 2 , getHeight() / 2, mTextPaint);		
-		canvas.drawRect(0, 0, getWidth(), getHeight(), mTextPaint);
+		
+		int width = getWidth();
+		int height = getHeight();
+		
+		canvas.drawText("00%", (width - mTextBounds.width()) / 2, height - (height - mTextBounds.height()) / 2, mTextPaint);		
+		canvas.drawRect(0, 0, width, height, mTextPaint);
+
+		if (width > height) {
+			mArcRect.set((width - height) / 2 + ARC_MARGIN , ARC_MARGIN, (width - height) / 2 + height - ARC_MARGIN, height - ARC_MARGIN);
+		} else {
+			mArcRect.set(ARC_MARGIN, (height - width ) / 2 + ARC_MARGIN , width - ARC_MARGIN, (height - width) / 2 + width - ARC_MARGIN);
+		}
+		
+		if (mMax > mMin) {
+			canvas.drawArc(mArcRect, 90, (mProgress - mMin) * 360 / (mMax - mMin), false, mArcPaint);
+		}
+			 
 	}
 	
 	@Override
