b9ded39fb5092f82afa6b0b345899aedd13af985
MrZeRo2000 <MrZeRo2000@outlook.com>
2015-11-09 16:43:26 +0200

    version 0.4.5

--- app/src/main/java/com/romanpulov/symphonytimer/activity/HistoryActivity.java
+++ app/src/main/java/com/romanpulov/symphonytimer/activity/HistoryActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Pair;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
