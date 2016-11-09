78c27194392c00268ff90e98befcaf84bec25034
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-11-09 17:11:24 +0200

    iss52 adjustment of MainActivity and SettingsActivity

--- app/src/main/java/com/romanpulov/symphonytimer/activity/SettingsActivity.java
+++ app/src/main/java/com/romanpulov/symphonytimer/activity/SettingsActivity.java
@@ -2,9 +2,13 @@ package com.romanpulov.symphonytimer.activity;
 
 import com.romanpulov.symphonytimer.R;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ListView;
 
 public class SettingsActivity extends ActionBarActivity {
     @Override
@@ -16,5 +20,17 @@ public class SettingsActivity extends ActionBarActivity {
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME, ActionBar.DISPLAY_SHOW_HOME);
         actionBar.setIcon(R.drawable.tuba);
+    }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        View view  = super.onCreateView(parent, name, context, attrs);
+
+        if (view != null) {
+            ListView preferencesListView = (ListView) view.findViewById(android.R.id.list);
+            preferencesListView.setPadding(0, 0, 0, 0);
+        }
+
+        return view;
     }
 }
