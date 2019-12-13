5dadf64ca172ab4ca77d9c3e8587a4a4297b5163
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-12-13 18:31:37 +0200

    added logging version

--- app/src/main/java/com/romanpulov/symphonytimer/loader/onedrive/BackupOneDriveUploader.java
+++ app/src/main/java/com/romanpulov/symphonytimer/loader/onedrive/BackupOneDriveUploader.java
@@ -6,6 +6,7 @@ import com.onedrive.sdk.core.ClientException;
 import com.onedrive.sdk.extensions.Item;
 import com.romanpulov.library.common.loader.core.AbstractContextLoader;
 import com.romanpulov.symphonytimer.R;
+import com.romanpulov.symphonytimer.helper.LoggerHelper;
 import com.romanpulov.symphonytimer.helper.db.DBStorageHelper;
 import com.romanpulov.library.onedrive.OneDriveHelper;
 import com.romanpulov.symphonytimer.loader.cloud.CloudLoaderRepository;
@@ -21,6 +22,10 @@ import static com.romanpulov.symphonytimer.cloud.OneDriveAccountRepository.ONE_D
 import static com.romanpulov.symphonytimer.common.NotificationRepository.NOTIFICATION_ID_LOADER;
 
 public class BackupOneDriveUploader extends AbstractContextLoader {
+    private void log(String message) {
+        LoggerHelper.logContext(mContext, "BackupOneDriveUploader", message);
+    }
+
     private final OneDriveHelper mOneDriveHelper;
     private final DBStorageHelper mDBStorageHelper;
     private CountDownLatch mLocker = new CountDownLatch(1);
@@ -34,6 +39,9 @@ public class BackupOneDriveUploader extends AbstractContextLoader {
     @Override
     public void load() throws Exception {
         final File[] files = mDBStorageHelper.getDatabaseBackupFiles();
+
+        log("Got backup files:" + files.toString());
+
         final AtomicReference<Exception> mException = new AtomicReference<>();
         final AtomicReference<Integer> mFileCounter = new AtomicReference<>();
         mFileCounter.set(files.length);
@@ -41,6 +49,7 @@ public class BackupOneDriveUploader extends AbstractContextLoader {
         mOneDriveHelper.setOnOneDriveItemListener(new OneDriveHelper.OnOneDriveItemListener() {
             @Override
             public void onItemSuccess(Item item) {
+                log("Item 1 success:" + item);
                 mOneDriveHelper.setOnOneDriveItemListener(new OneDriveHelper.OnOneDriveItemListener() {
                     @Override
                     public void onItemSuccess(Item item) {
@@ -52,6 +61,7 @@ public class BackupOneDriveUploader extends AbstractContextLoader {
 
                     @Override
                     public void onItemFailure(ClientException ex) {
+                        log("Putting file Exception:" + ex.getMessage());
                         mException.set(ex);
                         mLocker.countDown();
                     }
@@ -59,9 +69,11 @@ public class BackupOneDriveUploader extends AbstractContextLoader {
 
                 try {
                     for (File f : files) {
+                        log("Putting file:" + f.getName());
                         mOneDriveHelper.putFile(f, CloudLoaderRepository.REMOTE_PATH);
                     }
                 } catch (IOException e) {
+                    log("Putting file IO exception:" + e.getMessage());
                     mException.set(e);
                     mLocker.countDown();
                 }
@@ -69,12 +81,15 @@ public class BackupOneDriveUploader extends AbstractContextLoader {
 
             @Override
             public void onItemFailure(ClientException ex) {
+                log("Item 1 exception:" + ex.getMessage());
                 mException.set(ex);
                 mLocker.countDown();
             }
         });
 
+        log("Creating folder");
         mOneDriveHelper.createFolder(CloudLoaderRepository.REMOTE_PATH);
+        log("Waiting for folder creation");
         mLocker.await();
 
         if (mException.get() != null) {
