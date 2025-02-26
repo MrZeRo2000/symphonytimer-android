d826fa9a311cc8657360e3f2c55c04468a66bfaf
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-02-26 17:39:59 +0200

    iss#111 Migrate MainActivity to Navigation Component and Fragment: blank fragment and nav

---
+++ app/src/main/java/com/romanpulov/symphonytimer/fragment/MainFragment.java
@@ -0,0 +1,28 @@
+package com.romanpulov.symphonytimer.fragment;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.romanpulov.symphonytimer.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentMainBinding binding = FragmentMainBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+}
