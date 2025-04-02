61297a5ff26d0d6b3c1060c8dd9fa3481d609b20
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-04-02 17:32:27 +0300

    iss#111 Migrate MainActivity to Navigation Component and Fragment: HistoryFragment: added menu

--- app/src/main/java/com/romanpulov/symphonytimer/fragment/HistoryFragment.java
+++ app/src/main/java/com/romanpulov/symphonytimer/fragment/HistoryFragment.java
@@ -1,19 +1,26 @@
 package com.romanpulov.symphonytimer.fragment;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.view.*;
 import android.widget.ArrayAdapter;
 
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 
+import androidx.lifecycle.Lifecycle;
+import com.romanpulov.symphonytimer.R;
 import com.romanpulov.symphonytimer.databinding.FragmentHistoryBinding;
 import com.romanpulov.symphonytimer.model.DMTimers;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class HistoryFragment extends Fragment {
+    private static final String TAG = HistoryFragment.class.getSimpleName();
     private static final String ARG_TIMERS = "timers";
     private static final String ARG_HISTORY_FILTER_ID = "history_filter_id";
 
@@ -27,9 +34,10 @@ public class HistoryFragment extends Fragment {
     public static <T extends HistoryFragment> T newInstance(Class<T> historyFragmentClass, DMTimers dmTimers, int historyFilterId) {
         T fragment;
         try {
-            fragment = historyFragmentClass.newInstance();
-        } catch (java.lang.InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
+            fragment = historyFragmentClass.getDeclaredConstructor().newInstance();
+        } catch (java.lang.InstantiationException | IllegalAccessException | NoSuchMethodException |
+                 InvocationTargetException e) {
+            Log.e(TAG, "Error creating fragment from class", e);
             throw new RuntimeException(e.getMessage());
         }
         Bundle args = new Bundle();
@@ -62,5 +70,27 @@ public class HistoryFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        requireActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER);
+
+        MenuHost menuHost = requireActivity();
+        menuHost.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.history_options, menu);
+
+                MenuItem item = menu.findItem(R.id.spinner);
+                Spinner spinner = (Spinner) item.getActionView();
+                ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+                        requireContext(), R.array.history_filter, android.R.layout.simple_spinner_item);
+                spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(spinnerAdapter);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
     }
 }
